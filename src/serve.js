d89ba7c93128974b6ee371d87660e144031a4f41
ThanhTd <alimodosay@gmail.com>
2023-12-04 23:29:32 +0700

    update

--- src/serve.js
+++ src/serve.js
@@ -2,6 +2,7 @@ const express = require("express");
 require('dotenv').config()
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const mysql = require('mysql2')
 
 const app = express();
 const port = process.env.PORT ?? 8888;
@@ -10,6 +11,24 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app)
 
 app.use('/', webRoutes)
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "hoidanit",
+  port: 3307,
+  password: '123456'
+});
+
+connection.query(
+  'select * from Users u',
+  function (err, results, fields) {
+    console.log('result>>', results);
+    console.log('fields>>', fields);
+  }
+)
+
+
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
